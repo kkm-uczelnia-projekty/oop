2e88e8553f82ee728d1faeb32bb0e2e251972c6f
Jakub <kubasurdej@gmail.com>
2022-03-27 20:36:17 +0200

    Add timestamps to messages

--- lab3/Logger/WriterLogger.cs
+++ lab3/Logger/WriterLogger.cs
@@ -8,8 +8,15 @@ namespace ConsoleApp.Logger
         {
             foreach (string message in messages)
             {
-                writer.WriteLine(message);
+                var formattedMessage = FormatMessage(message);
+                writer.WriteLine(formattedMessage);
             }
+        }
+
+        protected virtual string FormatMessage(string message)
+        {
+            var timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz");
+            return String.Format("{0}: {1}", timestamp, message);
         }
 
         public virtual void Dispose()
