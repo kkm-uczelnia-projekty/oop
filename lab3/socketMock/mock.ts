72f2166a4d8f1be4e35f1c2a9ae0a90a2174d596
Jakub Surdej <jakub.surdej@hemfrid.se>
2022-04-02 18:44:18 +0200

    Simplify tcp mock server

--- lab3/socketMock/mock.ts
+++ lab3/socketMock/mock.ts
@@ -1,18 +1,8 @@
 #!/usr/bin/env ts-node
 
 import { createServer as createTcpServer } from "net";
-
-const server = createTcpServer((socket) => {
-  socket.on("connect", () => {
-    console.log("connect");
-  });
-
-  socket.on("ready", () => {
-    console.log("ready");
-  });
-
+createTcpServer((socket) => {
   socket.on("data", (data) => {
     console.log(data.toString("utf-8"));
   });
-});
-server.listen(1234);
+}).listen(1234);
